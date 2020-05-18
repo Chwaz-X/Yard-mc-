72d179510ef90e1d1f6a44251bc83a42e84eae37
Chwaz-X <64857216+Chwaz-X@users.noreply.github.com>
2020-05-19 02:19:15 +0300

    Add files via upload

--- Chwaz-X.sh
+++ Chwaz-X.sh
@@ -36,73 +36,78 @@ pkg install perl -y
 pkg install nano -y
 pkg install vim -y
 pkg install cat -y
-pkg install pip -y
+pkg insta
 pkg install nmap -y
 pkg install hydra  -y
 pkg install openssl -y
 apt install nodejs -y
 bash Chwaz-X.sh
 
-elif [[ $secim == 04 || $secim == 4 ]]; then
-clear
-echo -e '\e[37Hammer Kuruluyor'
-git clone https://github.com/cyweb/hammer
-echo -e '\e[31mHammer Kurulumu Biti'
-sleep 1.1
-bash Chwaz-X.sh
-
 elif [[ $secim == 01 || $secim == 1 ]]; then
 clear
+cd
 echo -e '\e[31mKurulum Başladı'
 git clone https://github.com/sqlmapproject/sqlmap
 echo -e '\e[37mKurulum Biti'
-sleep 1.1y
+cd Yard-mc-
 bash Chwaz-X.sh
 
 elif [[ $secim == 02 || $secim == 2 ]]; then
 clear
+cd
 echo -e '\e[31mKurulum Başladı'
 git clone https://github.com/Tuhinshubhra/RED_HAWK
-echo -e '\e[37mKurulum Biti'                                                  sleep 1.1y
+echo -e '\e[37mKurulum Biti'
+cd Yard-mc-
 bash Chwaz-X.sh
 
 elif [[ $secim == 03 || $secim == 3 ]]; then
 clear
+cd
 echo -e '\e[31mKurulum Başladı'
 git clone https://github.com/starskytr90/BOTNET
-echo -e '\e[37mKurulum Biti'                                                 >
+echo -e '\e[37mKurulum Biti'
+cd Yard-mc-
 bash Chwaz-X.sh
 
 elif [[ $secim == 04 || $secim == 4 ]]; then
 clear
+cd
 echo -e '\e[31mKurulum Başladı'
 git clone https://github.com/cyweb/hammer
-echo -e '\e[37mKurulum Biti'                                                 >
+echo -e '\e[37mKurulum Biti'
+cd Yard-mc-
 bash Chwaz-X.sh
 
 elif [[ $secim == 05 || $secim == 5 ]]; then
 clear
 echo -e '\e[31mKurulum Başladı'
+cd
 git clone https://github.com/Cesar-Hack-Gray/scam
-echo -e '\e[37mKurulum Biti'                                                 >
+echo -e '\e[37mKurulum Biti'
+cd Yard-mc-
 bash Chwaz-X.sh
 
 elif [[ $secim == 06 || $secim == 6 ]]; then
 clear
 echo -e '\e[31mKurulum Başladı'
+cd
 git clone https://github.com/fuck3erboy/instahack
-echo -e '\e[37mKurulum Biti'                                                 >
+echo -e '\e[37mKurulum Biti'
+cd Yard-mc-
 bash Chwaz-X.sh
 
 elif [[ $secim == 07 || $secim == 7 ]]; then
 clear
+cd
 echo -e '\e[31mKurulum Başladı'
 git clone https://github.com/joker25000/Optiva-Framework
-echo -e '\e[37mKurulum Biti'                                                 >
+echo -e '\e[37mKurulum Biti'
+cd Yard-mc-
 bash Chwaz-X.sh
 else
         clear
         echo -e'\e[31mHatalı Giriş Yaptınız'
         bash Chwaz-X.sh
 fi
-
+													
